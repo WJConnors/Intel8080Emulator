709db6a8fe70e6c08afaa58e85c652d2094e354b
WJConnors <37446973+WJConnors@users.noreply.github.com>
2025-03-04 17:00:17 +0000

    Basic MOV instructions and struct restructure

--- 8080func.h
+++ 8080func.h
@@ -2,7 +2,34 @@
 #ifndef _8080FUNC_H
 #define _8080FUNC_H
 
-#include "8080emulator.c"  // Include the necessary structures (like State8080)
+#include <stdint.h>  // Include the necessary structures (like State8080)
+
+
+typedef struct ConditionCodes {    
+    uint8_t    z:1;     //zero 
+    uint8_t    s:1;     //sign
+    uint8_t    p:1;     //parity
+    uint8_t    cy:1;    //carry
+    uint8_t    ac:1;    //auxiliary carry
+    uint8_t    pad:3;   //padding
+} ConditionCodes;
+
+typedef struct State8080 { 
+    //registers   
+    uint8_t    a;    
+    uint8_t    b;    
+    uint8_t    c;    
+    uint8_t    d;    
+    uint8_t    e;    
+    uint8_t    h;    
+    uint8_t    l;    
+    uint16_t    sp;    
+    uint16_t    pc;    
+    uint8_t*     memory; //ram pointers
+    struct      ConditionCodes      cc;    
+    uint8_t     int_enable; //interrupts enabled
+} State8080;
+
 
 // Declare the function to disassemble or emulate instructions
 void UnimplementedInstruction(State8080* state);
